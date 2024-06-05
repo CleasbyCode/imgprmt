9974e2b048c00a569ce16854415d0d7e65464ad6
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-06-05 23:13:35 +0100

    Update imgprmt.cpp

--- src/Windows_cli/imgprmt.cpp
+++ src/Windows_cli/imgprmt.cpp
@@ -11,12 +11,12 @@ void startImgprmt(const std::string& IMAGE_FILENAME) {
 
 	constexpr uint_fast8_t
 		JPG_SIG[]		{ 0xFF, 0xD8, 0xFF },
-		JPG_END_SIG[]	{ 0xFF, 0xD9 },
+		JPG_END_SIG[]		{ 0xFF, 0xD9 },
 		DQT_SIG[]		{ 0xFF, 0xDB },
 		EXIF_SIG[]		{ 0x45, 0x78, 0x69, 0x66, 0x00, 0x00, 0x49, 0x49 },
-		XPACKET_SIG[]	{ 0x78, 0x70, 0x61, 0x63, 0x6B, 0x65, 0x74, 0x20, 0x65, 0x6E, 0x64 },
-		ICC_KODAK_SIG[]	{ 0x4B, 0x4F, 0x44, 0x41, 0x52, 0x4F, 0x4D },
-		ICC_PROFILE_SIG[]{ 0x49, 0x43, 0x43, 0x5F, 0x50, 0x52, 0x4F, 0x46, 0x49, 0x4C, 0x45 };
+		XPACKET_SIG[]		{ 0x78, 0x70, 0x61, 0x63, 0x6B, 0x65, 0x74, 0x20, 0x65, 0x6E, 0x64 },
+		ICC_KODAK_SIG[]		{ 0x4B, 0x4F, 0x44, 0x41, 0x52, 0x4F, 0x4D },
+		ICC_PROFILE_SIG[]	{ 0x49, 0x43, 0x43, 0x5F, 0x50, 0x52, 0x4F, 0x46, 0x49, 0x4C, 0x45 };
 	
 	if (!std::equal(std::begin(JPG_SIG), std::end(JPG_SIG), std::begin(Image_Vec)) || !std::equal(std::begin(JPG_END_SIG), std::end(JPG_END_SIG), std::end(Image_Vec) - 2)) {
 		std::cerr << "\nImage File Error: This is not a valid JPG image.\n\n";
@@ -27,13 +27,11 @@ void startImgprmt(const std::string& IMAGE_FILENAME) {
 
 	const uint_fast32_t ICC_PROFILE_POS = searchFunc(Image_Vec, 0, 0, ICC_PROFILE_SIG);
 
-	if (Image_Vec.size() > ICC_PROFILE_POS) {
-		
+	if (Image_Vec.size() > ICC_PROFILE_POS) {	
 		const uint_fast32_t ICC_KODAK_POS = searchFunc(Image_Vec, ICC_PROFILE_POS, 0, ICC_KODAK_SIG);
 		if (Image_Vec.size() > ICC_KODAK_POS) {
 			isKodak_Profile = true;	// If an image has this profile, we need to put it back later, so as to retain image color quality.
 		}
-
 		Image_Vec.erase(Image_Vec.begin(), Image_Vec.begin() + ICC_PROFILE_POS);
 	}
 
@@ -68,8 +66,8 @@ void startImgprmt(const std::string& IMAGE_FILENAME) {
 
 	std::wcout << L"\nType or paste in your prompt as one long sentence. Add <br> tags for new lines, if required.\n\nImage Description: ";
 
-	constexpr uint_fast32_t wcin_buffer_size = 8000;  	// The default string length in Windows console for "wcin" when "_O_U16TEXT" is set, 
-														// seems to be just over 2000 characters. So we increase it here to 8000 to provide a more flexible size.
+	constexpr uint_fast32_t wcin_buffer_size = 8000;  // The default string length in Windows console for "wcin" when "_O_U16TEXT" is set, 
+					 		  // seems to be just over 2000 characters. So we increase it here to 8000 to provide a more flexible size.
 
 	wchar_t* wcin_buffer = new wchar_t[wcin_buffer_size]();
 
@@ -102,9 +100,9 @@ void startImgprmt(const std::string& IMAGE_FILENAME) {
 	delete[] wcin_buffer;
 
 	constexpr uint_fast16_t
-		MAX_PROFILE_SIZE = 10000,		
-		PROMPT_INSERT_INDEX = 3900,		// (Default Profile) Insert location within Profile_Vec of the HTML page for the users's prompt text.
-		LINK_INSERT_INDEX = 3757,		// (Default Profile) Insert location within Profile_Vec of the HTML page for the user's web link.
+		MAX_PROFILE_SIZE = 10000,	// X(Twitter) size limit ~10KB.	
+		PROMPT_INSERT_INDEX = 3900,	// (Default Profile) Insert location within Profile_Vec of the HTML page for the users's prompt text.
+		LINK_INSERT_INDEX = 3757,	// (Default Profile) Insert location within Profile_Vec of the HTML page for the user's web link.
 		KODAK_PROFILE_SIZE_DIFF = 775,	// Kodak Profile is 775 bytes greater than the Default Profile.
 		KODAK_PROMPT_INSERT_INDEX = PROMPT_INSERT_INDEX + KODAK_PROFILE_SIZE_DIFF, // (Kodak Profile) Insert location within Profile_Vec of the HTML page for the users's prompt text.
 		KODAK_LINK_INSERT_INDEX = LINK_INSERT_INDEX + KODAK_PROFILE_SIZE_DIFF;	   // (Kodak Profile) Insert location within Profile_Vec of the HTML page for the user's web link (url).
@@ -116,14 +114,13 @@ void startImgprmt(const std::string& IMAGE_FILENAME) {
 
 	uint_fast8_t
 		profile_internal_size_field = 40,	// Start index location for internal size field of the image icc profile.(Max four bytes, only two used).
-		profile_size_field = 22,			// Start index location for size field of the main image icc profile. (Max two bytes)
+		profile_size_field = 22,		// Start index location for size field of the main image icc profile. (Max two bytes)
 		bits = 16;
 		
 	isKodak_Profile 
 		? Profile_Vec.insert(Profile_Vec.begin() + PROFILE_INSERT_INDEX, Kodak_Profile_Vec.begin(), Kodak_Profile_Vec.end()) 
 		: Profile_Vec.insert(Profile_Vec.begin() + PROFILE_INSERT_INDEX, Default_Profile_Vec.begin(), Default_Profile_Vec.end());
 	
-
 	Kodak_Profile_Vec.clear();
 	Default_Profile_Vec.clear();
 
