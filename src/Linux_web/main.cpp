38f3bbbff6a0a069cc6f970abbb59fb8f6d90bd9
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-09-30 19:18:37 +0100

    Update main.cpp

--- src/Linux_web/main.cpp
+++ src/Linux_web/main.cpp
@@ -1,6 +1,6 @@
-﻿//	imgprmt v1.0.4 (Linux edition for imgprmt website ). Created by Nicholas Cleasby (@CleasbyCode) 19/05/2023
-//	To be used only with https://cleasbycode.co.uk/imgprmt/app/
-//	If you want a imgprmt CLI tool, then use the Windows edition from the github page.
+﻿//	imgprmt v1.4 (Linux edition for imgprmt website ). Created by Nicholas Cleasby (@CleasbyCode) 19/05/2023
+//	To be used ONLY with Web App https://cleasbycode.co.uk/imgprmt/app/
+//	If you want the imgprmt CLI tool, then download/compile the imgprmt Windows edition from the github page.
 
 #include "imgprmt.h"
 
@@ -8,19 +8,18 @@ int main(int argc, char** argv) {
 	if (argc == 2) {
 		const std::string IMAGE_FILENAME = std::string(argv[1]);
 		
-		std::string file_extension = IMAGE_FILENAME.length() > 3 ? IMAGE_FILENAME.substr(IMAGE_FILENAME.length() - 4) : IMAGE_FILENAME;
+		const std::filesystem::path IMAGE_PATH(IMAGE_FILENAME);
+		const std::string IMAGE_EXTENSION = IMAGE_PATH.extension().string();
 
-		file_extension = file_extension == "jpeg" || file_extension == "jfif" ? ".jpg" : file_extension;
-
-		if (file_extension == ".jpg" && std::filesystem::exists(IMAGE_FILENAME)) {
-			startImgprmt(IMAGE_FILENAME);
-		} else {
-			std::cerr << (file_extension != ".jpg"
-				? "\nFile Type Error: Invalid file extension found. Expecting only \"jpg\""
-				: "\nImage File Error: File not found. Check the filename and try again")
-			<< ".\n\n";
-
-			std::exit(EXIT_FAILURE);
+		if (IMAGE_EXTENSION != ".jpg" && IMAGE_EXTENSION != ".jpeg" && IMAGE_EXTENSION != ".jfif") {
+			std::cerr << "\nFile Type Error: Invalid file extension. Expecting only \"jpg, jpeg or jfif\" image extensions.\n\n";
+			return 1;
 		}
+
+		if (!std::filesystem::exists(IMAGE_FILENAME)) {
+			std::cerr << "\nImage File Error: File not found. Check the filename and try again.\n\n";
+			return 1;
+		}
+	imgPrmt(IMAGE_FILENAME);
 	}
 }
