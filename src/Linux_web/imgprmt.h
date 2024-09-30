1c82bde295a5dc422f594aefb0ef7cbe4cab8c07
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-09-30 19:12:27 +0100

    Update imgprmt.h

--- src/Linux_web/imgprmt.h
+++ src/Linux_web/imgprmt.h
@@ -3,19 +3,23 @@
 #include <algorithm>
 #include <filesystem>
 #include <cstdint>
+#include <regex>
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
 
 #include "searchFunc.cpp"
+#include "eraseSegments.cpp"
 #include "profileData.cpp"
 #include "valueUpdater.cpp"
 #include "imgprmt.cpp"
 
 template <uint_fast8_t N>
-uint_fast32_t searchFunc(std::vector<uint_fast8_t>&, uint_fast32_t, uint_fast8_t, const uint_fast8_t (&)[N]);
+uint_fast32_t searchFunc(std::vector<uint8_t>&, uint_fast32_t, uint_fast8_t, const uint_fast8_t (&)[N]);
 
-void
-	startImgprmt(const std::string&),
-	valueUpdater(std::vector<uint_fast8_t>&, uint_fast32_t, const uint_fast32_t, uint_fast8_t);
+void	
+	eraseSegments(std::vector<uint8_t>&, bool&),
+	valueUpdater(std::vector<uint8_t>&, uint_fast32_t, const uint_fast32_t, uint_fast8_t);
+
+uint_fast8_t imgPrmt(const std::string&);
