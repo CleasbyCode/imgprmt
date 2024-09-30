2b76a55bac50e3bd9db5d584b598ff57211f2a87
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-09-30 19:03:52 +0100

    Update valueUpdater.cpp

--- src/Linux_web/valueUpdater.cpp
+++ src/Linux_web/valueUpdater.cpp
@@ -1,4 +1,4 @@
-void valueUpdater(std::vector<uint_fast8_t>& vec, uint_fast32_t value_insert_index, const uint_fast32_t VALUE, uint_fast8_t bits) {
+void valueUpdater(std::vector<uint8_t>& vec, uint_fast32_t value_insert_index, const uint_fast32_t VALUE, uint_fast8_t bits) {
 	while (bits) {
 		vec[value_insert_index++] = (VALUE >> (bits -= 8)) & 0xff;
 	}
