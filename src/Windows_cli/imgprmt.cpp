d7e228a98644c2400c9f3bd728b219edf5ef673b
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2025-03-07 02:56:51 +0000

    Update imgprmt.cpp

--- src/Windows_cli/imgprmt.cpp
+++ src/Windows_cli/imgprmt.cpp
@@ -35,25 +35,22 @@ int imgPrmt(const std::string& IMAGE_FILENAME, ArgOption platform) {
 
 	fflush(stdin); fflush(stdout);
 
-	std::ignore = _setmode(_fileno(stdin), 0x20000);  // Set standard input for Windows to "_O_U16TEXT" so our wide strings contain correct wide characters.
-	std::ignore = _setmode(_fileno(stdout), 0x20000); // Set standard output for Windows to "_O_U16TEXT" so the console displays the correct wide characters.
+	std::ignore = _setmode(_fileno(stdin), 0x20000);  
+	std::ignore = _setmode(_fileno(stdout), 0x20000); 
 
 	std::wcout << L"\nType or paste in your prompt as one long sentence. Add <br> tags for new lines, if required.\n\nImage Description: ";
 
-	constexpr uint32_t wcin_buffer_size = 8000;  	// The default string length in Windows console for "wcin" when "_O_U16TEXT" is set, 
-							// seems to be just over 2000 characters. So we increase it here to 8000 to provide a more flexible size.
-
+	constexpr uint32_t wcin_buffer_size = 8000;  	
+							
 	wchar_t* wcin_buffer = new wchar_t[wcin_buffer_size]();
 
-	std::wcin.rdbuf()->pubsetbuf(wcin_buffer, wcin_buffer_size); // Set the new buffer size, as above.
+	std::wcin.rdbuf()->pubsetbuf(wcin_buffer, wcin_buffer_size); 
 
 	std::wcin.getline(wcin_buffer, wcin_buffer_size);
 	std::wstring wide_prompt(wcin_buffer);
 
-	// Search the wide string for problem characters and replace them with html entity codes.
 	replaceProblemChars(wide_prompt);
 
-	// Determine the required buffer size for the multibyte string
 	uint32_t bufferSize = WideCharToMultiByte(CP_UTF8, 0, wide_prompt.c_str(), -1, nullptr, 0, nullptr, nullptr);
 
 	if (!bufferSize) {
@@ -61,10 +58,8 @@ int imgPrmt(const std::string& IMAGE_FILENAME, ArgOption platform) {
 		return 1;
 	}
 
-	// Allocate memory for the multibyte string
 	char* multiByteString = new char[bufferSize];
 
-	// Convert the wide character string to multibyte (UTF-8) string
 	if (WideCharToMultiByte(CP_UTF8, 0, wide_prompt.c_str(), -1, multiByteString, bufferSize, nullptr, nullptr) == 0) {
 		std::wcerr << L"Error: Conversion failed\n";
 		delete[] multiByteString;
@@ -73,35 +68,28 @@ int imgPrmt(const std::string& IMAGE_FILENAME, ArgOption platform) {
 
 	delete[] wcin_buffer;
 
-	constexpr uint16_t MAX_SEGMENT_SIZE = 10000;				// Segment size limit.
+	constexpr uint16_t MAX_SEGMENT_SIZE = 10000;
 
 	const uint16_t
-		PROMPT_INSERT_INDEX 	= hasBlueskyOption ? 0xE76 : 0xF3C,	// Insert location within Segment containing HTML page for the user's prompt text.
-		LINK_INSERT_INDEX 	= hasBlueskyOption ? 0xDE7 : 0xEAD;	// Insert location within Segment containing HTML page for the user's web link.
+		PROMPT_INSERT_INDEX 	= hasBlueskyOption ? 0xE76 : 0xF3C,	
+		LINK_INSERT_INDEX 	= hasBlueskyOption ? 0xDE7 : 0xEAD;	
 
-	// For X/Twitter, Mastodon, Tumblr, Flickr.
 	constexpr uint8_t 
-		PROFILE_INTERNAL_DIFF 	= 38,	 // Bytes we don't count as part of internal profile size.
-		PROFILE_MAIN_DIFF 	= 22,	 // Bytes we don't count as part of profile size.
-		PROFILE_INSERT_INDEX 	= 0x14;	 // Insert location within Default_Vec for the color profile (Segment_Vec). X/Twitter, Mastodon, Tumblr, Flickr.
+		PROFILE_INTERNAL_DIFF 	= 38,	 
+		PROFILE_MAIN_DIFF 	= 22,	 
+		PROFILE_INSERT_INDEX 	= 0x14;	 
 	
 	uint8_t
-		// For Twitter, Mastodon, Tumblr, Flickr.
-		profile_size_field_index = 0x28, // Start index location for internal size field of the image color profile.(Max four bytes, only two used).
-
-		segment_size_field_index = hasBlueskyOption ? 0x04 : 0x16,  // Start index location for size field of the main segment EXIF or color profile. (Max two bytes)
-
+		profile_size_field_index = 0x28, 
+		segment_size_field_index = hasBlueskyOption ? 0x04 : 0x16,  
 		exif_segment_xres_offset_field_index = 0x2A,
-		exif_segment_xres_offset_size_diff = 0x36, // Always 0x36 size difference between EXIF segment size.
-
+		exif_segment_xres_offset_size_diff = 0x36, 
 		exif_segment_yres_offset_field_index = 0x36,
-		exif_segment_yres_offset_size_diff = 0x2E, // Always 0x2E size difference between EXIF segment size.
-
+		exif_segment_yres_offset_size_diff = 0x2E, 
 		exif_segment_artist_size_field_index = 0x4A,
-		exif_segment_size_diff = 0x91, // EXIF segment size - artist comment section size.
-
+		exif_segment_size_diff = 0x91, 
 		exif_segment_subifd_offset_index = 0x5A,
-		exif_segment_subifd_offset_size_diff = 0x26, // Always 0x26 size difference between EXIF segment size.
+		exif_segment_subifd_offset_size_diff = 0x26, 
 		bits = 16;
 		
 	if (!hasBlueskyOption) {
@@ -110,7 +98,6 @@ int imgPrmt(const std::string& IMAGE_FILENAME, ArgOption platform) {
 		segment_vec.swap(bluesky_vec);	
 	}
 
-	// Insert prompt & the url link into their relevant index positions of the html section, within vector Segment_Vec.
 	segment_vec.insert(segment_vec.begin() + PROMPT_INSERT_INDEX, multiByteString, multiByteString + bufferSize - 1);
 	segment_vec.insert(segment_vec.begin() + LINK_INSERT_INDEX, url_link.begin(), url_link.end());
 
@@ -124,17 +111,15 @@ int imgPrmt(const std::string& IMAGE_FILENAME, ArgOption platform) {
 	uint32_t segment_size = static_cast<uint32_t>(segment_vec.size()); 
 
 	if (hasBlueskyOption) {
-		// For Bluesky segment size don't count/include the JPG ID + APP ID "FFD8FFE1" (4 bytes).
 		segment_size -= 4;
-
-		// Update EXIF segment size field (FFE1xxxx)
+		
 		valueUpdater(segment_vec, segment_size_field_index, segment_size, bits);
 		
 		bits = 32;
 		uint32_t 
 			exif_xres_offset = segment_size - exif_segment_xres_offset_size_diff,
 			exif_yres_offset = segment_size - exif_segment_yres_offset_size_diff,
-			exif_artist_size = (segment_size - exif_segment_size_diff) + 4, // For this variable, include the JPG ID + APP ID "FFD8FFE1" (4 bytes).
+			exif_artist_size = (segment_size - exif_segment_size_diff) + 4, 
 			exif_subifd_offset = segment_size - exif_segment_subifd_offset_size_diff;
 			
 		valueUpdater(segment_vec, exif_segment_xres_offset_field_index, exif_xres_offset, bits);
@@ -142,16 +127,11 @@ int imgPrmt(const std::string& IMAGE_FILENAME, ArgOption platform) {
 		valueUpdater(segment_vec, exif_segment_artist_size_field_index, exif_artist_size, bits); 
 		valueUpdater(segment_vec, exif_segment_subifd_offset_index, exif_subifd_offset, bits);
 	} else {
-		// Update color profile segment size field (FFE2xxxx)
 		valueUpdater(segment_vec, segment_size_field_index, segment_size - PROFILE_MAIN_DIFF, bits);
-
-		// Update internal color profile size field
 		valueUpdater(segment_vec, profile_size_field_index, segment_size - PROFILE_INTERNAL_DIFF, bits);
 	}
-
 	image_vec.insert(image_vec.begin(), segment_vec.begin(), segment_vec.end());
 	
-	// Clear	
 	std::vector<uint8_t>().swap(segment_vec);
 
 	const uint32_t IMAGE_SIZE = static_cast<uint32_t>(image_vec.size());
