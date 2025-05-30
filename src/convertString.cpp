cf1fd6fba3adfc766b8e5df7c3d1467f57eb48e0
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2025-05-30 11:55:40 +0100

    Update convertString.cpp

--- src/convertString.cpp
+++ src/convertString.cpp
@@ -1,4 +1,3 @@
-
 #include <stdexcept>
 
 #ifdef _WIN32
@@ -9,25 +8,25 @@
 #endif
 
 std::string convertString(const std::wstring& wide) {
-#ifdef _WIN32
-    if (wide.empty()) return {};
+    #ifdef _WIN32
+        if (wide.empty()) return {};
 
-    int size = WideCharToMultiByte(CP_UTF8, 0, wide.c_str(), -1, nullptr, 0, nullptr, nullptr);
-    if (size <= 0) throw std::runtime_error("WideCharToMultiByte failed.");
+        int size = WideCharToMultiByte(CP_UTF8, 0, wide.c_str(), -1, nullptr, 0, nullptr, nullptr);
+        if (size <= 0) throw std::runtime_error("WideCharToMultiByte failed.");
 
-    std::string convertedString(size - 1, 0); // Exclude null terminator
-    WideCharToMultiByte(CP_UTF8, 0, wide.c_str(), -1, convertedString.data(), size, nullptr, nullptr);
-    return convertedString;
+        std::string convertedString(size - 1, 0); 
+        WideCharToMultiByte(CP_UTF8, 0, wide.c_str(), -1, convertedString.data(), size, nullptr, nullptr);
+        return convertedString;
 
-#else
-    std::mbstate_t state{};
-    const wchar_t* src = wide.data();
-    size_t len = std::wcsrtombs(nullptr, &src, 0, &state);
-    if (len == static_cast<size_t>(-1)) throw std::runtime_error("Conversion to UTF-8 failed.");
+    #else
+        std::mbstate_t state{};
+        const wchar_t* src = wide.data();
+        size_t len = std::wcsrtombs(nullptr, &src, 0, &state);
+        if (len == static_cast<size_t>(-1)) throw std::runtime_error("Conversion to UTF-8 failed.");
 
-    std::string convertedString(len, 0);
-    src = wide.data(); // reset pointer
-    std::wcsrtombs(convertedString.data(), &src, len, &state);
-    return convertedString;
-#endif
+        std::string convertedString(len, 0);
+        src = wide.data(); // reset pointer
+        std::wcsrtombs(convertedString.data(), &src, len, &state);
+        return convertedString;
+    #endif
 }
