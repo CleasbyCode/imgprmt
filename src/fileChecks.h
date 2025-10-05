9503c82cf2c540fcd79522f1cf1aa19058d4c6ab
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2025-10-05 09:45:19 +0100

    Delete src/fileChecks.h

--- src/fileChecks.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "programArgs.h"
-#include <vector>
-#include <cstdint>
-
-bool hasValidImageExtension(const std::string&);
-bool hasValidFilename(const std::string&);
-void validateImageFile(std::string&, ArgOption, uintmax_t&, std::vector<uint8_t>&);
