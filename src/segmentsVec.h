3ffd54fb6a0d954739fef548d7748a00b8823663
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2025-10-05 09:52:46 +0100

    Delete src/segmentsVec.h

--- src/segmentsVec.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <vector>
-#include <cstdint>
-
-extern std::vector<uint8_t> profile_vec;
-extern std::vector<uint8_t> segment_vec;
-extern std::vector<uint8_t> twitter_vec;
-extern std::vector<uint8_t> bluesky_vec;
