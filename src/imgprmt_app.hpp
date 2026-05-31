c9e04f09319c536001bf15a560307ca7778bd9c4
Nicholas Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2026-05-31 20:10:36 +0100

    Delete src directory

--- src/imgprmt_app.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string_view>
-
-namespace imgprmt {
-inline constexpr std::string_view VERSION = "1.3";
-
-int run_cli(int argc, char** argv);
-}  // namespace imgprmt
--- src/imgprmt_url.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <string>
-
-namespace imgprmt::url {
-void validate_url_link_core(const std::string& url);
-}  // namespace imgprmt::url
